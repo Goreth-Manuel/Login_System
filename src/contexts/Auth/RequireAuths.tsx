582d5c861e73f76f51acdcf86a7b1ff45ab0402f
Goreth Manuel <gorethmanuel0@gmail.com>
2025-04-22 22:34:51 +0100

    Adiciona um redirecionamento para quando o usuário não estiver logado :sparkles:

--- src/contexts/Auth/RequireAuths.tsx
+++ src/contexts/Auth/RequireAuths.tsx
@@ -1,5 +1,12 @@
-import { JSX } from "react"
+import { JSX, useContext } from "react"
+import { AuthContext } from "./AuthContext";
+import { Login } from "../../pages/Login";
 
 export const RequireAuths = ({children}: {children: JSX.Element}) => {
+    const auth = useContext(AuthContext)
+
+    if(!auth.user) {
+        return <Login/>
+    }
     return children;
 }                       
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,31 @@
+import { useState } from "react"
+
 export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+
+    const handleLogin = () => {
+        
+    }
+
     return (
         <div>
             <h2>Página fechada</h2>
-            <button>Fazer login</button>
+           
+            <input 
+                type="text" 
+                value={email} 
+                placeholder="Digite o seu email" 
+                name="" id="" 
+            />
+
+            <input 
+                type="password" 
+                value={password} 
+                placeholder="Digite sua password" 
+                name="" id="" 
+            />
+            <button onClick={handleLogin}>Logar</button>
         </div>
       
     )
