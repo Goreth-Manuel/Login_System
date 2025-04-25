716b74e3e356dbaeca74485972e1b8225d9907a2
Goreth Manuel <gorethmanuel0@gmail.com>
2025-04-25 22:29:26 +0100

    Atualização do useEffect que valida o token :sparkles:

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,18 +5,21 @@ import { useNavigate } from "react-router-dom";
 export const Login = () => {
 
     const auth = useContext(AuthContext)
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
 
     const handleLogin = async () => {
+        console.log("Função executada com sucesso")
         if(email && password) {
             const isLogged = await auth.signin(email, password);
             if(isLogged) {
-                navigate('/');
+                navigate('/')
+            console.log("Navegação realizada com sucesso")
+
             } else {
-                alert("credencias inválidas");
+                alert('credencias inválidas.');
             }
         }
         
